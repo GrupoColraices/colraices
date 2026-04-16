e7d5775570ea365826c04698ea281d8be1521dba
Grupo Colraices <juancamilo.rivera@agenciamomentum.co>
2026-04-16 16:37:40 -0500

    arreglos3

--- app/components/layout/SiteLayout.tsx
+++ app/components/layout/SiteLayout.tsx
@@ -4,9 +4,20 @@ import Footer from "./Footer";
 
 export default function SiteLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen bg-background text-foreground">
+    <div className="min-h-screen flex flex-col bg-background text-foreground">
       <Navbar />
-      <main className="pt-[69px]">{children}</main>
+
+      <main
+        className="
+        flex-1
+        pt-[69px]
+        sm:pt-[72px]
+        md:pt-[76px]
+      "
+      >
+        {children}
+      </main>
+
       <Footer />
     </div>
   );
