c2e7dc5999a5b437a1a62870e0fd2a2f88c7566a
Grupo Colraices <juancamilo.rivera@agenciamomentum.co>
2026-04-22 18:25:01 -0500

    buena-data90%

--- app/finanzas/buena-data/page.tsx
+++ app/finanzas/buena-data/page.tsx
@@ -1,6 +1,5 @@
 import Breadcrumb from "@/app/components/layout/Breadcrumb";
 import BdHero from "@/app/components/buena-data/BdHero";
-import SiteLayout from "@/app/components/layout/SiteLayout";
 import Buenadatasection from "@/app/components/buena-data/Buenadatasection";
 import ServicioIncluye from "@/app/components/buena-data/ServicioIncluye";
 import DuracionProceso from "@/app/components/buena-data/DuracionProceso";
@@ -8,10 +7,9 @@ import Garantias from "@/app/components/buena-data/Garantias";
 import FAQSectionBuenaD from "@/app/components/buena-data/FAQSectionBuenaD";
 import CTASection from "@/app/components/buena-data/CTASection";
 
-
 export default function BuenaDataPage() {
   return (
-    <SiteLayout>
+    <>
       <Breadcrumb />
       <BdHero />
       <Buenadatasection />
@@ -20,6 +18,6 @@ export default function BuenaDataPage() {
       <Garantias />
       <FAQSectionBuenaD />
       <CTASection />
-    </SiteLayout>
+    </>
   );
 }
