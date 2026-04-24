77fd088573f2e0db3cbe44178c0d19a618d748e1
Grupo Colraices <juancamilo.rivera@agenciamomentum.co>
2026-04-24 14:14:06 -0500

    brujula-financiera20%

--- app/finanzas/brujula-financiera/page.tsx
+++ app/finanzas/brujula-financiera/page.tsx
@@ -2,12 +2,16 @@ import Breadcrumb from "@/app/components/layout/Breadcrumb";
 import SiteLayout from "@/app/components/layout/SiteLayout";
 
 import HeroDiagnostico from "@/app/components/brujula-financiera/HeroDiagnostico";
+import ProblemaSolucionSection from "@/app/components/brujula-financiera/ProblemaSolucionSection";
+
 
 export default function BrujulaFinanPage() {
   return (
     <SiteLayout>
       <Breadcrumb />
       <HeroDiagnostico />
+      <ProblemaSolucionSection />
+      
     </SiteLayout>
   );
 }
