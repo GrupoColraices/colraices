a34cc944262c766c61199dc7a6f5140306680402
Grupo Colraices <juancamilo.rivera@agenciamomentum.co>
2026-05-13 17:39:04 -0500

    errores91%

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import Script from "next/script";
 import "./globals.css";
 import { Montserrat } from "next/font/google";
 import FloatingContactButton from "./components/layout/FloatingContactButton";
@@ -8,6 +9,8 @@ const montserrat = Montserrat({
   weight: ["300", "400", "600", "700"],
 });
 
+const GTM_ID = "GTM-WQDR37KG";
+
 export default function RootLayout({
   children,
 }: {
@@ -16,6 +19,25 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={montserrat.className}>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {children}
         <FloatingContactButton />
       </body>
