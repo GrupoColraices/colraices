6aea6bdc5ba3736dd53f1dc58cc7150383de7b1a
Grupo Colraices <juancamilo.rivera@agenciamomentum.co>
2026-05-23 15:10:49 -0500

    pruebas arquitectura y link

--- app/(legal)/legal-y-migratorio/page.tsx
+++ app/(legal)/legal-y-migratorio/page.tsx
@@ -2,9 +2,9 @@ import Breadcrumb from "@/app/components/layout/Breadcrumb";
 import HeroLegal from "@/app/components/legal-migracion/HeroLegal";
 
 import ServiciosSection from "@/app/components/legal-migracion/ServiciosSection";
-import ComoTrabajamos from "../components/legal-migracion/Como-Trabajamos";
-import PreguntasFrecuentes from "../components/legal-migracion/PreguntasFrecuentes";
-import CtaBannerSection from "../components/legal-migracion/CtaBannerSection";
+import ComoTrabajamos from "@/app/components/legal-migracion/Como-Trabajamos";
+import PreguntasFrecuentes from "@/app/components/legal-migracion/PreguntasFrecuentes";
+import CtaBannerSection from "@/app/components/legal-migracion/CtaBannerSection";
 import SiteLayout from "@/app/components/layout/SiteLayout";
 
 export default function LegalMigracionPage() {
